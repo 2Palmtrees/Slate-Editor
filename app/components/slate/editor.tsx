2e6d52dd14d61dccb5a147a112ed9c402ff3a8c7
2Palmtrees <2palmtrees@protonmail.com>
2025-07-25 15:46:28 +0200

    comment

--- app/components/slate/editor.tsx
+++ app/components/slate/editor.tsx
@@ -100,6 +100,7 @@ export default function SlateEditor({ item }: { item: Item }) {
 
     setHasBackground((prev) => !prev);
 
+    // Invert colors on some table elements.
     Transforms.setNodes(
       editor,
       { invert: !hasBackground },
